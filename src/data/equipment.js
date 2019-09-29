dc79146fece584f3d1d16acad8069a775a696e16
rxdey <jyh1994@qq.com>
2019-09-29 11:04:53 +0800

    - add 基础装备

---
+++ src/data/equipment.js
@@ -0,0 +1,70 @@
+const baseUrl = '/image/base/';
+const items = {
+  base: [
+    {
+      id: 1,
+      name: '暴风大剑',
+      description: '+15攻击力',
+      icon: 'BF.png'
+    },
+    {
+      id: 2,
+      name: '无用大棒',
+      description: '+20%技能伤害',
+      icon: 'Rod.png'
+    },
+    {
+      id: 3,
+      name: '反曲之弓',
+      description: '+20%攻速',
+      icon: 'Bow.png'
+    },
+    {
+      id: 4,
+      name: '女神之泪',
+      description: '+20法力值',
+      icon: 'Tear.png'
+    },
+    {
+      id: 5,
+      name: '锁子甲',
+      description: '+20护甲',
+      icon: 'Vest.png'
+    },
+    {
+      id: 6,
+      name: '负极斗篷',
+      description: '+20魔抗',
+      icon: 'Cape.png'
+    },
+    {
+      id: 7,
+      name: '巨人腰带',
+      description: '+200生命值',
+      icon: 'Belt.png'
+    },
+    {
+      id: 8,
+      name: '拳套',
+      description: '+10暴击概率、+10%闪避概率',
+      icon: 'Glove.png'
+    },
+    {
+      id: 9,
+      name: '金铲铲',
+      description: '可以做点什么...',
+      icon: 'Spatula.png'
+    }
+  ],
+  equipment: [
+    {
+      id: 100,
+      name: '',
+      description: '',
+      icon: '',
+      relation: []
+    }
+  ]
+};
+
+export default items;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,32 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="Home">
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld,
+  name: 'Home',
+  props: {
   },
+  components: {
+  },
+  data () {
+    return {
+    };
+  },
+  created () {
+  },
+  mounted () {
+  },
+  methods: {
+  },
+  watch: {
+  },
+  computed: {
+  }
 };
 </script>
+
+<style lang="less">
+</style>
